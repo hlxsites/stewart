9903c11db49d861b909e0b0ece37d3471f40f8e4
Ramachandra Avuthu <ramachandra@Ramachandras-MacBook-Pro-2.local>
2023-07-20 09:25:21 -0400

    tab size reduced to 2

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,18 +1,18 @@
 export default function decorate(block) {
-    const elementContainer = block.querySelector(':scope > div > div');
+  const elementContainer = block.querySelector(':scope > div > div');
 
-    const heroWrapper = document.createElement('div');
-    elementContainer.querySelector('picture') && heroWrapper.append(elementContainer.querySelector('picture'));
-    elementContainer.querySelector('h1') && heroWrapper.append(elementContainer.querySelector('h1'));
-    elementContainer.querySelector('h2') && heroWrapper.append(elementContainer.querySelector('h2'));
-    if(elementContainer.querySelector('a')) {
-        const anchorWrapper = document.createElement('div');
-        anchorWrapper.classList.add('align-end');
-        anchorWrapper.append(elementContainer.querySelector('a'));
-        heroWrapper.append(anchorWrapper);
-    }
-      
-    block.append(heroWrapper);
+  const heroWrapper = document.createElement('div');
+  elementContainer.querySelector('picture') && heroWrapper.append(elementContainer.querySelector('picture'));
+  elementContainer.querySelector('h1') && heroWrapper.append(elementContainer.querySelector('h1'));
+  elementContainer.querySelector('h2') && heroWrapper.append(elementContainer.querySelector('h2'));
+  if(elementContainer.querySelector('a')) {
+    const anchorWrapper = document.createElement('div');
+    anchorWrapper.classList.add('align-end');
+    anchorWrapper.append(elementContainer.querySelector('a'));
+    heroWrapper.append(anchorWrapper);
+  }
+    
+  block.append(heroWrapper);
 
-    elementContainer.parentElement.remove();
+  elementContainer.parentElement.remove();
 }
