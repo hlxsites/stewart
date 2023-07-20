e0bae6bb5c1fcab254d4fb3f2b032f7f4b33c1f0
Ramachandra Avuthu <ramachandra@Ramachandras-MacBook-Pro-2.local>
2023-07-20 09:34:18 -0400

    lint issues fixed

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -2,16 +2,22 @@ export default function decorate(block) {
   const elementContainer = block.querySelector(':scope > div > div');
 
   const heroWrapper = document.createElement('div');
-  elementContainer.querySelector('picture') && heroWrapper.append(elementContainer.querySelector('picture'));
-  elementContainer.querySelector('h1') && heroWrapper.append(elementContainer.querySelector('h1'));
-  elementContainer.querySelector('h2') && heroWrapper.append(elementContainer.querySelector('h2'));
-  if(elementContainer.querySelector('a')) {
+  if (elementContainer.querySelector('picture')) {
+    heroWrapper.append(elementContainer.querySelector('picture'));
+  }
+  if (elementContainer.querySelector('h1')) {
+    heroWrapper.append(elementContainer.querySelector('h1'));
+  }
+  if (elementContainer.querySelector('h2')) {
+    heroWrapper.append(elementContainer.querySelector('h2'));
+  }
+  if (elementContainer.querySelector('a')) {
     const anchorWrapper = document.createElement('div');
     anchorWrapper.classList.add('align-end');
     anchorWrapper.append(elementContainer.querySelector('a'));
     heroWrapper.append(anchorWrapper);
   }
-    
+
   block.append(heroWrapper);
 
   elementContainer.parentElement.remove();
