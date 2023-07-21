08bf2bb5c15d4a19b0758d16ba0596b53e8a709e
Ramachandra Avuthu <ramachandra@Ramachandras-MacBook-Pro-2.local>
2023-07-21 15:13:37 -0400

    footer markup updated

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,29 @@
 import { readBlockConfig } from '../../scripts/lib-franklin.js';
-import { createElement, decorateIcons } from '../../scripts/scripts.js';
+import { createElement, decorateIcons, wrapImgsInLinks } from '../../scripts/scripts.js';
+
+function createIconsList(ele) {
+  const list = document.createElement('ul');
+  list.className = 'icons-wrapper';
+  [...ele.children].forEach((icon) => {
+    const listItem = document.createElement('li');
+    listItem.append(icon.querySelector('a'));
+    list.append(listItem);
+  });
+
+  return list;
+}
+
+function createLinksList(ele) {
+  const list = document.createElement('ul');
+  list.className = 'links-list';
+  [...ele.children].forEach((p) => {
+    const listItem = document.createElement('li');
+    listItem.append(p.querySelector('a'));
+    list.append(listItem);
+  });
+
+  return list;
+}
 
 /**
  * loads and decorates the footer
@@ -18,9 +42,37 @@ export default async function decorate(block) {
 
     // decorate footer DOM
     const footer = createElement('div');
+    footer.className = 'inner-wrapper';
     footer.innerHTML = html;
 
     decorateIcons(footer);
+
+    const firstColumn = document.createElement('div');
+    firstColumn.className = 'column-1';
+    const secondColumn = document.createElement('div');
+    secondColumn.className = 'column-2';
+
+    [...footer.children].forEach((ele, index, array) => {
+      if (array.length - 1 === index) {
+        const iconsList = createIconsList(ele);
+        secondColumn.append(iconsList);
+      } else {
+        if (ele.querySelector('picture') && ele.querySelector('a')) {
+          wrapImgsInLinks(ele);
+          ele.className = 'logo';
+        }
+        if (!ele.querySelector('picture') && ele.querySelector('a')) {
+          const linskList = createLinksList(ele);
+          ele.className = 'links-wrapper';
+          ele.replaceChildren(linskList);
+        }
+        if (!ele.querySelector('picture') && !ele.querySelector('a')) {
+          ele.className = 'legal-text';
+        }
+        firstColumn.append(ele);
+      }
+    });
+    footer.replaceChildren(firstColumn, secondColumn);
     block.append(footer);
   }
 }
