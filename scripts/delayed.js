c9cab807a210508ba39e7709185bf509c0044549
Sean Steimer <ssteimer@adobe.com>
2023-08-03 09:54:50 -0700

    add active group checking

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -10,10 +10,21 @@ function loadGoogleTagManager(placeholders) {
   }
 }
 
+function OTLoaded() {
+  if (window.OnetrustActiveGroups) {
+    const activeGroups = window.OnetrustActiveGroups.split(',');
+    // eslint-disable-next-line no-console
+    console.log(`OneTrust Loaded. Active groups: ${activeGroups}`);
+    // use active groups to determine what else is loaded
+    // down the line, if blocks need to rely on this, e.g marketo form block
+    // we could send an event here that blocks could listen on to trigger their loading.
+  }
+}
+
 function loadOneTrust(placeholders) {
   const otId = placeholders.onetrustId;
   if (otId) {
-    window.OptanonWrapper = () => { };
+    window.OptanonWrapper = OTLoaded;
 
     loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', {
       type: 'text/javascript',
