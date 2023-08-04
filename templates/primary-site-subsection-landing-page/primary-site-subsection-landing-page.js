1f03822fe2b6b94728d6c9891de15e49a653dfd9
Sean Steimer <ssteimer@adobe.com>
2023-08-04 15:37:49 -0700

    section nav code

---
+++ templates/primary-site-subsection-landing-page/primary-site-subsection-landing-page.js
@@ -0,0 +1,21 @@
+import { buildAutoBlocks, createElement } from '../../scripts/scripts.js';
+import { buildBlock } from '../../scripts/lib-franklin.js';
+
+/**
+ * build the section nav and place it after the hero
+ * @param {Element} main the main element
+ */
+export function buildSectionNav(main) {
+  const navSection = createElement('div');
+  const block = buildBlock('section-nav', [
+    ['level', 'parent'],
+  ]);
+  navSection.append(block);
+  const heroSection = main.querySelector('div');
+  heroSection.insertAdjacentElement('afterend', navSection);
+}
+
+export default async function buildTemplateAutoBlocks(main) {
+  buildAutoBlocks(main);
+  buildSectionNav(main);
+}
