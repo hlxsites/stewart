3f3269bb2369778919c7f897801770af5aaae64d
Sean Steimer <ssteimer@adobe.com>
2023-08-17 09:51:33 -0700

    add guard for mailto

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -165,8 +165,10 @@ export function decorateLinks(element) {
     try {
       if (a.href) {
         const url = new URL(a.href);
-        const hostMatch = hosts.some((host) => url.hostname.includes(host));
+        // protect against maito: links or other weirdness
+        if (url.protocol !== 'https:' && url.protocol !== 'http:') return;
 
+        const hostMatch = hosts.some((host) => url.hostname.includes(host));
         if (hostMatch) {
           // local links are rewritten to be relative
           a.href = `${url.pathname.replace('.html', '')}${url.search}${url.hash}`;
