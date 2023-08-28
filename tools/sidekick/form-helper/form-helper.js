b5df0771e239bd576698e712a84c1fdcf8feda03
Brendan Robert <brobert@adobe.com>
2023-08-28 18:04:11 -0500

    General fix-ups for form helper

--- tools/sidekick/form-helper/form-helper.js
+++ tools/sidekick/form-helper/form-helper.js
@@ -21,15 +21,20 @@ inputs.forEach((input) => {
     input.value = name;
     }
   });
+  return false;
 }
   
 function registerForm(form) {
-    const action = form.getAttribute('action');
-    const registerAction = `https://admin.hlx.page/form/hlxsites/stewart/main${action}.json`;
+    const actionUrl = form.getAttribute('action');
+    // strip off the first part of the url before /forms
+    const action = actionUrl.substring(actionUrl.indexOf('/forms'));
+    const registerAction = `https://admin.hlx.page/form/hlxsites/stewart/main${action}`;
     // Set form action to register action
     form.setAttribute('action', registerAction);
     // Trigger form submission
     try {
+        form.setAttribute('target', '_blank');
+        form.setAttribute('method', 'POST');
         form.submit();
     } finally {
         // Reset form action
@@ -37,21 +42,35 @@ function registerForm(form) {
     }
 }
 
-async function init() {
+function registerForms() {
+    const buttonStyle = 'width: 10em; background-color: var(--clr-green); border-radius: 5px; color: var(--clr-white); margin-left: 0.5em;width: 150px; height: 50px; color: white';
+
     window.top.document.querySelectorAll('form').forEach(form => {
+        if (form.isRegistered) return;
         // Locate the submit button
-        const submitButton = form.querySelector('button[type="submit"]');
+        const submitButton = form.querySelector('button[type="submit"],input[type="submit"]');
         // Append a button to trigger auto-fill
         const autoFillButton = document.createElement('button');
         autoFillButton.innerText = 'Auto-fill';
         autoFillButton.onclick = () => autofillForm(form);
-        submitButton.insertAdjacentElement('afterend', autoFillButton);
+        autoFillButton.style.cssText = buttonStyle;
+        submitButton.parentElement.append(autoFillButton);
         // Append a button to trigger form registration
         const registerButton = document.createElement('button');
         registerButton.innerText = 'Register Franklin';
         registerButton.onclick = () => registerForm(form);
-        submitButton.insertAdjacentElement('afterend', registerButton);
+        registerButton.style.cssText = buttonStyle;
+        submitButton.parentElement.append(registerButton);
+        form.isRegistered = true;
     });
+}
+
+
+async function init() {
+    const registerFormButton = document.createElement('button');
+    registerFormButton.onclick = registerForms;
+    registerFormButton.innerText = 'Register forms';
+    document.body.appendChild(registerFormButton);
 }
 
 init();
