9a38609a72074226112175ccbdde4bad67190465
Vijay Kumar J <jalagari@adobe.com>
2023-09-06 23:51:57 +0530

    Adding Form Block

---
+++ blocks/form1/decorators/index.js
@@ -0,0 +1,11 @@
+import { applyRuleEngine } from '../rules/index.js';
+
+export const transformers = [
+  applyRuleEngine,
+];
+
+export const asyncTransformers = [
+];
+
+export const requestTransformers = [
+];
